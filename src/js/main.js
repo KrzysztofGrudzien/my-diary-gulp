33962a461f6731d35ed6059ec65a42c679f90355
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-07-02 07:16:17 +0200

    feat: add content files and images

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,45 @@
-"use strict";
+'use strict';
 
-import moment from 'moment';
+// Replacement script - start
+const btn = document.querySelectorAll('.diary-tasks__more');
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', () => {
+    alert("tutaj będzie JS");
+  });
+}
+// Replacement script - end
+
+//=================================//
+/* Fill up a content of the footer */
+//=================================//
+const footer = document.querySelector('.footer');
+
+const addFooter = function (name) {
+  //create footer
+  const footerLink = `<a class="footer__link" href = "http://criscode.pl">${name}</a>`;
+  footer.innerHTML = `Copyright &copy 2019 - all rights reserved by ${footerLink} `;
+}
+addFooter('Krzysztof Grudzień');
+
+//=================================//
+/* Open and close navigation       */
+//=================================//
+const toogleIcon = document.querySelector('.header__nav-open-icon--js');
+const navContainer = document.querySelector('.nav');
+
+const displayMenu = () => {
+  navContainer.classList.toggle('hidden');
+}
+toogleIcon.addEventListener('click', displayMenu, false);
+
+//=====================================//
+/* Display name and age of the author  */
+//=====================================//
+const authorName = document.querySelector('.author__header--js');
+
+const showData = (name, lastName, yearOfBorn) => {
+  authorName.innerHTML = `${name} <span class="bolder">${lastName}</span> <sup>${yearOfBorn}</sup>`;
+
+}
+showData('Krzysztof', 'Grudzień', '84\'');
